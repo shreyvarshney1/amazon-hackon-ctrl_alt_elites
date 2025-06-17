28ba4b8cd1a902ce836c8ccad164ee998830297c
manojkp08 <manojkpr92806@gmail.com>
2025-06-17 14:35:12 +0530

    fixed bugs in signup/page.tsx and page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "../components/ui/button"
-import { Input } from "../components/ui/input"
-import { Label } from "../components/ui/label"
-import { Card, CardContent, CardHeader } from "../components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import Link from "next/link"
 
 export default function AmazonSignupPage() {
