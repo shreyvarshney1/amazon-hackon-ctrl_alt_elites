75255e8be03e50d68f96e767bd4c858aa00dd62d
Sujoy De <sujoydeofficial@gmail.com>
2025-06-17 20:24:03 +0530

    minor fix

--- api/schemas.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    email = fields.Email(required=True)
